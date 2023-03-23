04963fa41ec84d7d0ac0bdff3e2dee4417e1f586
rwalrondch <rwalrond@companieshouse.gov.uk>
2023-03-23 12:07:52 +0000

    DACT-392 adding a sorry there is a problem service page.

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -3,13 +3,8 @@ import { logger } from "../utils/logger";
 import { Templates } from "../types/template.paths";
 
 const pageNotFound = (req: Request, res: Response) => {
-//  return res.status(404).render(Templates.ERROR_404, { templateName: Templates.ERROR_404 });
-  return res.status(404).render(Templates.SERVICE_OFFLINE_MID_JOURNEY, { templateName: Templates.SERVICE_OFFLINE_MID_JOURNEY });
-};
-const errorHandler401 = (err: Error, req: Request, res: Response, _next: NextFunction) => {
-  logger.errorRequest(req, `An error has occurred. Re-routing to the error screen - ${err.stack}`);
-  res.status(401).render(Templates.SERVICE_OFFLINE_MID_JOURNEY, { templateName: Templates.SERVICE_OFFLINE_MID_JOURNEY });
-};
+  return res.status(404).render(Templates.ERROR_404, { templateName: Templates.ERROR_404 });
+  };
 
 /**
  * This handler catches any other error thrown within the application.
