2ba5dfa19b180ee362ce1d04d5a7a858657202ba
anona-w <117649383+anona-w@users.noreply.github.com>
2023-08-23 12:15:34 +0100

    Added unit tests

--- src/controllers/appoint.director.submitted.controller.ts
+++ src/controllers/appoint.director.submitted.controller.ts
@@ -1,7 +1,8 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Templates } from "../types/template.paths";
+import { urlUtils } from "../utils/url";
 
-export const get = async (res: Response, next: NextFunction) => {
+export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
     return res.render(Templates.APPOINT_DIRECTOR_SUBMITTED, {
       templateName: Templates.APPOINT_DIRECTOR_SUBMITTED,
