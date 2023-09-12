8c22f4532871a24cda285c41b7192e51d216ac5a
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-09-12 11:57:55 +0100

    Improve test coverage

--- test/controllers/director.occupation.controller.unit.ts
+++ test/controllers/director.occupation.controller.unit.ts
@@ -71,7 +71,7 @@ describe("Director occupation controller tests", () => {
         expect(response.text).toContain("Astronaut");
       });
 
-      it("Should display director name on the page", async () => {
+      it("Should display full director name on the page", async () => {
         mockGetOfficerFiling.mockResolvedValueOnce({
           firstName: "Jim",
           middleNames: "Mid",
@@ -82,6 +82,18 @@ describe("Director occupation controller tests", () => {
 
         expect(response.text).toContain(PAGE_HEADING);
         expect(response.text).toContain("Jim Mid Smith");
+      });
+
+      it("Should display director name without middle name on the page", async () => {
+        mockGetOfficerFiling.mockResolvedValueOnce({
+          firstName: "Jim",
+          lastName: "Smith"
+        });
+
+        const response = await request(app).get(DIRECTOR_OCCUPATION_URL);
+
+        expect(response.text).toContain(PAGE_HEADING);
+        expect(response.text).toContain("Jim Smith");
       });
 
     });
