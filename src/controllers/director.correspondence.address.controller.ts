a259dfc0dc160d4c08c3c428aab8c40b2b01d440
olaych <oolatunji@companieshouse.gov.uk>
2023-10-12 14:16:00 +0100

    initial commit to update template

--- src/controllers/director.correspondence.address.controller.ts
+++ src/controllers/director.correspondence.address.controller.ts
@@ -2,12 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import { DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH, DIRECTOR_OCCUPATION_PATH } from "../types/page.urls";
 import { Templates } from "../types/template.paths";
 import { urlUtils } from "../utils/url";
+import { Session } from "@companieshouse/node-session-handler";
+import { getOfficerFiling } from "../services/officer.filing.service";
+import { formatTitleCase, retrieveDirectorNameFromFiling } from "../utils/format";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
+    const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
+    const session: Session = req.session as Session;
+
+    const officerFiling = await getOfficerFiling(session, transactionId, submissionId);
+
     return res.render(Templates.DIRECTOR_CORRESPONDENCE_ADDRESS, {
       templateName: Templates.DIRECTOR_CORRESPONDENCE_ADDRESS,
       backLinkUrl: urlUtils.getUrlToPath(DIRECTOR_OCCUPATION_PATH, req),
+      directorName: formatTitleCase(retrieveDirectorNameFromFiling(officerFiling)),
+      ...officerFiling.serviceAddress
     });
   } catch (e) {
     return next(e);
