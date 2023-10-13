85fabed5059634e6291422d23507b69cb71ff0cd
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-10-13 15:09:24 +0100

    Add stop screen for dissolved company

--- src/controllers/appoint.director.check.answers.controller.ts
+++ src/controllers/appoint.director.check.answers.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { DIRECTOR_PROTECTED_DETAILS_PATH, APPOINT_DIRECTOR_SUBMITTED_PATH, CURRENT_DIRECTORS_PATH } from "../types/page.urls";
+import { DIRECTOR_PROTECTED_DETAILS_PATH, APPOINT_DIRECTOR_SUBMITTED_PATH, CURRENT_DIRECTORS_PATH, URL_QUERY_PARAM, BASIC_STOP_PAGE_PATH } from "../types/page.urls";
 import { Templates } from "../types/template.paths";
 import { urlUtils } from "../utils/url";
 import { getCompanyProfile } from "../services/company.profile.service";
@@ -8,6 +8,8 @@ import { getOfficerFiling } from "../services/officer.filing.service";
 import { Session } from "@companieshouse/node-session-handler";
 import { formatTitleCase, retrieveDirectorNameFromFiling } from "../utils/format";
 import { toReadableFormat } from "../utils/date";
+import { getCurrentOrFutureDissolved } from "../services/stop.page.validation.service";
+import { STOP_TYPE } from "../utils/constants";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   const companyNumber= urlUtils.getCompanyNumberFromRequestParams(req);
@@ -33,6 +35,15 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
-  const nextPageUrl = urlUtils.getUrlToPath(APPOINT_DIRECTOR_SUBMITTED_PATH, req);
+  const companyNumber= urlUtils.getCompanyNumberFromRequestParams(req);
+  const session: Session = req.session as Session;
+  var nextPageUrl = urlUtils.getUrlToPath(APPOINT_DIRECTOR_SUBMITTED_PATH, req);
+  
+  if (await getCurrentOrFutureDissolved(session, companyNumber)){
+    nextPageUrl = urlUtils.getUrlToPath(BASIC_STOP_PAGE_PATH, req);
+    nextPageUrl = urlUtils.setQueryParam(nextPageUrl, URL_QUERY_PARAM.PARAM_STOP_TYPE, STOP_TYPE.DISSOLVED);
+  }
+
+
   return res.redirect(nextPageUrl);
 };
