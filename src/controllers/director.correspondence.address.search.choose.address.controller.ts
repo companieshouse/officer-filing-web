ce5d5a07495d2b4c51942a849ba744627c42a104
jditchburn <jditchburn@companieshouse.gov.uk>
2023-10-19 15:59:41 +0100

    DACT-800 Fix duplicated lines

--- src/controllers/director.correspondence.address.search.choose.address.controller.ts
+++ src/controllers/director.correspondence.address.search.choose.address.controller.ts
@@ -21,40 +21,40 @@ import { getCountryFromKey } from "../utils/web";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const session: Session = req.session as Session;
     const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
     const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
-    const session: Session = req.session as Session;
 
     const officerFiling: OfficerFiling = await getOfficerFiling(session, transactionId, submissionId);
     const postalCode = officerFiling?.serviceAddress?.postalCode;
     if (!postalCode) {
       throw new Error("Postal code is undefined");
     }
-    const ukAddresses: UKAddress[] = await getUKAddressesFromPostcode(POSTCODE_ADDRESSES_LOOKUP_URL, postalCode.replace(/\s/g, ''));
+    const addresses: UKAddress[] = await getUKAddressesFromPostcode(POSTCODE_ADDRESSES_LOOKUP_URL, postalCode.replace(/\s/g, ''));
     
-    return renderPage(req, res, officerFiling, ukAddresses, []);
+    return renderPage(req, res, officerFiling, addresses, []);
   } catch (e) {
     return next(e);
   }
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
+  const session: Session = req.session as Session;
   const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
   const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
-  const session: Session = req.session as Session;
 
   const officerFiling: OfficerFiling = await getOfficerFiling(session, transactionId, submissionId);
   const postalCode = officerFiling?.serviceAddress?.postalCode;
   if (!postalCode) {
     throw new Error("Postal code is undefined");
   }
-  const ukAddresses: UKAddress[] = await getUKAddressesFromPostcode(POSTCODE_ADDRESSES_LOOKUP_URL, postalCode.replace(/\s/g, ''));
+  const addresses: UKAddress[] = await getUKAddressesFromPostcode(POSTCODE_ADDRESSES_LOOKUP_URL, postalCode.replace(/\s/g, ''));
 
   const selectedPremises = req.body[DirectorField.ADDRESS_ARRAY];
-  const selectedAddress = ukAddresses.find((address: UKAddress) => address.premise === selectedPremises);
+  const selectedAddress = addresses.find((address: UKAddress) => address.premise === selectedPremises);
   if (!selectedAddress) {
-    const validationError = createValidationError(correspondenceAddressErrorMessageKey.CORRESPONDENCE_ADDRESS_BLANK, [DirectorField.ADDRESS_ARRAY], ukAddresses[0]?.premise);
-    return renderPage(req, res, officerFiling, ukAddresses, [validationError]);
+    const validationError = createValidationError(correspondenceAddressErrorMessageKey.CORRESPONDENCE_ADDRESS_BLANK, [DirectorField.ADDRESS_ARRAY], addresses[0]?.premise);
+    return renderPage(req, res, officerFiling, addresses, [validationError]);
   }
 
   const patchFiling: OfficerFiling = {
@@ -83,7 +83,7 @@ const renderPage = async (req: Request, res: Response, officerFiling: OfficerFil
       premises: address.premise,
       formattedAddress: formatTitleCase(address.premise + " " + address.addressLine1 + (address.addressLine2 ? ", " + address.addressLine2 : "") + ", " + address.postTown + ", " + getCountryFromKey(address.country)) + ", " + address.postcode
     };
-  }).sort((a, b) => (a.premises > b.premises) ? 1 : -1);
+  });
 
   return res.render(Templates.DIRECTOR_CORRESPONDENCE_CHOOSE_ADDRESS, {
     templateName: Templates.DIRECTOR_CORRESPONDENCE_CHOOSE_ADDRESS,
