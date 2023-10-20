fbce064df20c21da612f4bc37ac984a10df1e753
olaych <oolatunji@companieshouse.gov.uk>
2023-10-20 10:04:19 +0100

    added api-sdk-node changes

--- test/controllers/director.correspondence.address.controller.unit.ts
+++ test/controllers/director.correspondence.address.controller.unit.ts
@@ -65,7 +65,7 @@ describe("Director correspondence address controller tests", () => {
         const response = await request(app).get(PAGE_URL);
         expect(response.text).toContain(PAGE_HEADING);
         expect(response.text).toContain(directorNameMock.firstName);
-              expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.addressLineOne);
+        expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.addressLineOne);
         expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.locality);
         expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.region);
         expect(response.text).toContain(validCompanyProfile.registeredOfficeAddress.postalCode);
@@ -87,6 +87,21 @@ describe("Director correspondence address controller tests", () => {
         expect(response.text).not.toContain(PAGE_HEADING);
         expect(response.text).toContain(ERROR_PAGE_HEADING)
       });
+      
+      it(`should render ${DIRECTOR_CORRESPONDENCE_ADDRESS} with mapped registered office address `, async () => {
+        mockGetCompanyProfile.mockResolvedValue({});
+        mockGetOfficerFiling.mockResolvedValueOnce({
+          ...directorNameMock
+        });
+        const response = await request(app).get(PAGE_URL);
+        expect(response.text).toContain(PAGE_HEADING);
+        expect(response.text).not.toContain(validCompanyProfile.registeredOfficeAddress.addressLineOne);
+        expect(response.text).not.toContain(validCompanyProfile.registeredOfficeAddress.locality);
+        expect(response.text).not.toContain(validCompanyProfile.registeredOfficeAddress.postalCode);
+        expect(response.text).toContain(PUBLIC_REGISTER_INFORMATION);
+        expect(response.text).toContain(ACCORDION_INFORMATION);
+        expect(response.text).toContain(DIRECTOR_OCCUPATION_PATH_END)
+      })
     });
 
     describe("post tests", () => {
