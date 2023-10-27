cba642b37ce58b2f1ab47b0f852bf05751f77514
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-10-27 16:34:04 +0100

    Add get tests for correspondence link page

---
+++ test/controllers/director.correspondence.address.link.controller.unit.ts
@@ -0,0 +1,203 @@
+jest.mock("../../src/utils/feature.flag")
+jest.mock("../../src/services/officer.filing.service");
+jest.mock("../../src/services/company.profile.service");
+
+import mocks from "../mocks/all.middleware.mock";
+import request from "supertest";
+import app from "../../src/app";
+import { Request } from "express";
+import { Session } from "@companieshouse/node-session-handler";
+import { getOfficerFiling, patchOfficerFiling } from "../../src/services/officer.filing.service";
+import { buildValidationErrors } from "../../src/validation/protected.details.validation";
+import { ValidationError } from "../../src/model/validation.model";
+import { protectedDetailsErrorMessageKey } from "../../src/utils/api.enumerations.keys";
+
+import { directorAppliedToProtectDetailsValue } from "../../src/controllers/director.protected.details.controller";
+import { DirectorField } from "../../src/model/director.model";
+
+import { 
+  DIRECTOR_PROTECTED_DETAILS_PATH,
+  APPOINT_DIRECTOR_CHECK_ANSWERS_PATH,
+  DIRECTOR_CONFIRM_RESIDENTIAL_ADDRESS_PATH_END,
+  DIRECTOR_RESIDENTIAL_ADDRESS_PATH_END,
+  urlParams, 
+  DIRECTOR_CORRESPONDENCE_ADDRESS_LINK_PATH,
+  DIRECTOR_RESIDENTIAL_ADDRESS_PATH
+} from "../../src/types/page.urls";
+import { isActiveFeature } from "../../src/utils/feature.flag";
+
+const mockIsActiveFeature = isActiveFeature as jest.Mock;
+mockIsActiveFeature.mockReturnValue(true);
+const mockGetOfficerFiling = getOfficerFiling as jest.Mock;
+const mockPatchOfficerFiling = patchOfficerFiling as jest.Mock;
+const mockDirectorAppliedToProtectDetailsValue = directorAppliedToProtectDetailsValue as jest.Mock;
+
+const COMPANY_NUMBER = "12345678";
+const TRANSACTION_ID = "11223344";
+const SUBMISSION_ID = "55555555";
+const PAGE_HEADING = "If the registered office address changes in the future, do you want this to apply to the director&#39;s correspondence address too?";
+const ERROR_PAGE_HEADING = "Sorry, there is a problem with this service";
+
+const PAGE_URL = DIRECTOR_CORRESPONDENCE_ADDRESS_LINK_PATH
+  .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
+  .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
+  .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
+const NEXT_PAGE_URL = DIRECTOR_RESIDENTIAL_ADDRESS_PATH
+  .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
+  .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
+  .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
+
+const mockReq = {
+  session: {} as Session,
+  headers: {},
+  route: '',
+  method: '',
+  body: {
+    protected_details: 'false'
+  },
+} as Request;
+
+describe("Director correspondence address link controller tests", () => {
+
+    beforeEach(() => {
+      mocks.mockSessionMiddleware.mockClear();
+      mockGetOfficerFiling.mockClear();
+    });
+  
+    describe("get tests", () => {
+  
+      it("Should navigate to correspondence address link page with no radio buttons selected", async () => {
+        mockGetOfficerFiling.mockResolvedValueOnce({
+          title: "testTitle",
+          firstName: "testFirst",
+          middleNames: "testMiddle",
+          lastName: "testLast",
+          formerNames: "testFormer"
+        })
+        
+        const response = await request(app).get(PAGE_URL);
+  
+        expect(response.text).toContain(PAGE_HEADING);
+        expect(response.text).toContain("Testfirst Testmiddle Testlast");
+        expect(response.text).toContain('value="sa_to_roa_yes" aria-describedby');
+        expect(response.text).toContain('value="sa_to_roa_no" aria-describedby');
+      });
+
+      it("Should navigate to correspondence address link page with yes radio selected", async () => {
+        mockGetOfficerFiling.mockResolvedValueOnce({
+          title: "testTitle",
+          firstName: "testFirst",
+          middleNames: "testMiddle",
+          lastName: "testLast",
+          formerNames: "testFormer",
+          isMailingAddressSameAsRegisteredOfficeAddress: true
+        })
+        
+        const response = await request(app).get(PAGE_URL);
+  
+        expect(response.text).toContain('value="sa_to_roa_yes" checked');
+      });
+
+      it("Should navigate to correspondence address link page with no radio selected", async () => {
+        mockGetOfficerFiling.mockResolvedValueOnce({
+          title: "testTitle",
+          firstName: "testFirst",
+          middleNames: "testMiddle",
+          lastName: "testLast",
+          formerNames: "testFormer",
+          isMailingAddressSameAsRegisteredOfficeAddress: false
+        })
+        
+        const response = await request(app).get(PAGE_URL);
+  
+        expect(response.text).toContain('value="sa_to_roa_no" checked');
+      });
+
+      it("Should navigate to error page when feature flag is off", async () => {
+        mockIsActiveFeature.mockReturnValueOnce(false);
+        const response = await request(app).get(PAGE_URL);
+  
+        expect(response.text).toContain(ERROR_PAGE_HEADING);
+      });
+
+      it("Should catch error when officer filing returned error ", async () => {
+        mockGetOfficerFiling.mockRejectedValueOnce(new Error("Error getting officer filing"));
+        
+        const response = await request(app).get(PAGE_URL);
+        expect(response.text).toContain(ERROR_PAGE_HEADING);
+      });
+
+    });
+
+    // describe("post tests", () => {
+    //   it("should redirect to appoint director check answers page when there are no errors", async () => {
+    //     mockGetOfficerFiling.mockResolvedValueOnce({
+    //       directorName: "Test Director"
+    //     })
+    //     //mockBuildValidationErrors.mockReturnValueOnce([]);
+    //     mockPatchOfficerFiling.mockResolvedValueOnce({data:{
+    //     }});
+
+    //     const response = await request(app).post(PAGE_URL);
+
+    //     expect(response.text).toContain("Found. Redirecting to " + NEXT_PAGE_URL);
+    //   });
+
+    //   it("should display an error when no radio button is selected", async () => {
+    //     mockGetOfficerFiling.mockResolvedValueOnce({
+    //       directorName: "Test Director"
+    //     })
+        
+    //     let validationErrorsResponse: ValidationError[] = [
+    //       {
+    //         messageKey: protectedDetailsErrorMessageKey.NO_PROTECTED_DETAILS_RADIO_BUTTON_SELECTED,
+    //         source: ['protected_details'],
+    //         link: 'protected_details'
+    //       }
+    //     ];
+    //     //mockBuildValidationErrors.mockReturnValueOnce(validationErrorsResponse);
+    //     mockPatchOfficerFiling.mockResolvedValueOnce({data:{
+    //     }});
+
+    //     const response = await request(app).post(PAGE_URL);
+
+    //     expect(response.text).toContain(protectedDetailsErrorMessageKey.NO_PROTECTED_DETAILS_RADIO_BUTTON_SELECTED);
+    //   });
+
+    //   it("should catch error if patch officer filing failed", async () => {
+    //     const mockPatchOfficerFilingResponse = {
+    //     };
+    //     mockPatchOfficerFiling.mockResolvedValueOnce(mockPatchOfficerFilingResponse);
+    //     const response = (await request(app).post(PAGE_URL).send({}));
+    //     expect(response.text).not.toContain(protectedDetailsErrorMessageKey.NO_PROTECTED_DETAILS_RADIO_BUTTON_SELECTED);
+    //     expect(response.text).not.toContain(PAGE_HEADING);
+    //     expect(response.text).toContain(ERROR_PAGE_HEADING)
+    //   });
+    // });
+
+    // describe("directorAppliedToProtectDetailsValue", () => {
+    //   it("should return undefined if directorProtectedDetailsRadio is not defined", () => {
+    //     const req = { body: {} } as Request;
+    //     const result = mockDirectorAppliedToProtectDetailsValue(req);
+    //     expect(result).toBeUndefined();
+    //   });
+    
+    //   it("should return true if directorProtectedDetailsRadio is 'yes'", () => {
+    //     const req = { body: { [DirectorField.PROTECTED_DETAILS_RADIO]: DirectorField.PROTECTED_DETAILS_YES } } as Request;
+    //     const result = mockDirectorAppliedToProtectDetailsValue(req);
+    //     expect(result).toBe(true);
+    //   });
+    
+    //   it("should return false if directorProtectedDetailsRadio is 'no'", () => {
+    //     const req = { body: { [DirectorField.PROTECTED_DETAILS_RADIO]: DirectorField.PROTECTED_DETAILS_NO } } as Request;
+    //     const result = mockDirectorAppliedToProtectDetailsValue(req);
+    //     expect(result).toBe(false);
+    //   });
+    
+    //   it("should return undefined if directorProtectedDetailsRadio is not 'yes' or 'no'", () => {
+    //     const req = { body: { [DirectorField.PROTECTED_DETAILS_RADIO]: "invalid" } } as Request;
+    //     const result = mockDirectorAppliedToProtectDetailsValue(req);
+    //     expect(result).toBeUndefined();
+    //   });
+    // });
+});
