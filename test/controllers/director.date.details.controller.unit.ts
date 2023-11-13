6edf58f90f7858252d5bace99f9694512b9137e1
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-11-13 12:04:50 +0000

    Combine unit tests for dob and appointed pages into date details page

--- test/controllers/director.date.details.controller.unit.ts
+++ test/controllers/director.date.details.controller.unit.ts
@@ -6,13 +6,13 @@ import mocks from "../mocks/all.middleware.mock";
 import request from "supertest";
 import app from "../../src/app";
 
-import { DIRECTOR_APPOINTED_DATE_PATH, DIRECTOR_DATE_OF_BIRTH_PATH, urlParams } from "../../src/types/page.urls";
+import { DIRECTOR_DATE_DETAILS_PATH, DIRECTOR_NATIONALITY_PATH, urlParams } from "../../src/types/page.urls";
 import { isActiveFeature } from "../../src/utils/feature.flag";
 import { getOfficerFiling, patchOfficerFiling } from "../../src/services/officer.filing.service";
 import { getValidationStatus } from "../../src/services/validation.status.service";
 import { mockValidValidationStatusResponse, mockValidationStatusError, mockValidationStatusErrorDob } from "../mocks/validation.status.response.mock";
 import { ValidationStatusResponse } from "@companieshouse/api-sdk-node/dist/services/officer-filing";
-import { buildValidationErrors } from "../../src/controllers/director.date.of.birth.controller";
+import { buildValidationErrors } from "../../src/controllers/director.date.details.controller";
 import { dobDateErrorMessageKey } from "../../src/utils/api.enumerations.keys";
 import * as apiEnumerations from "../../src/utils/api.enumerations";
 
@@ -26,18 +26,18 @@ const COMPANY_NUMBER = "12345678";
 const TRANSACTION_ID = "11223344";
 const SUBMISSION_ID = "55555555";
 const APPOINTMENT_ID = "987654321";
-const PAGE_HEADING = "What is the director&#39;s date of birth?";
+const PAGE_HEADING = "Date details";
 const ERROR_PAGE_HEADING = "Sorry, there is a problem with this service";
-const DIRECTOR_DATE_OF_BIRTH_URL = DIRECTOR_DATE_OF_BIRTH_PATH
+const DIRECTOR_DATE_DETAILS_URL = DIRECTOR_DATE_DETAILS_PATH
   .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
   .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
   .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
-const DIRECTOR_APPOINTED_DATE_URL = DIRECTOR_APPOINTED_DATE_PATH
+const DIRECTOR_NATIONALITY_URL = DIRECTOR_NATIONALITY_PATH
   .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
   .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
   .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
 
-describe("Director date of birth controller tests", () => {
+describe("Director date details controller tests", () => {
 
     beforeEach(() => {
       mocks.mockAuthenticationMiddleware.mockClear();
@@ -48,19 +48,19 @@ describe("Director date of birth controller tests", () => {
   
     describe("get tests", () => {
   
-      it("Should navigate to director date of birth page", async () => {
+      it("Should navigate to director date details page", async () => {
         mockGetOfficerFiling.mockResolvedValueOnce({
           referenceAppointmentId: APPOINTMENT_ID
         });
 
-        const response = await request(app).get(DIRECTOR_DATE_OF_BIRTH_URL);
+        const response = await request(app).get(DIRECTOR_DATE_DETAILS_URL);
   
         expect(response.text).toContain(PAGE_HEADING);
       });
 
       it("Should navigate to error page when feature flag is off", async () => {
         mockIsActiveFeature.mockReturnValueOnce(false);
-        const response = await request(app).get(DIRECTOR_DATE_OF_BIRTH_URL);
+        const response = await request(app).get(DIRECTOR_DATE_DETAILS_URL);
   
         expect(response.text).toContain(ERROR_PAGE_HEADING);
       });
@@ -68,15 +68,19 @@ describe("Director date of birth controller tests", () => {
       it("Should populate filing data on the page", async () => {
         mockGetOfficerFiling.mockResolvedValueOnce({
           referenceAppointmentId: APPOINTMENT_ID,
-          dateOfBirth: "1993-08-15"
+          dateOfBirth: "1993-08-15",
+          appointedOn: "2021-09-10"
         });
 
-        const response = await request(app).get(DIRECTOR_DATE_OF_BIRTH_URL);
+        const response = await request(app).get(DIRECTOR_DATE_DETAILS_URL);
   
         expect(response.text).toContain(PAGE_HEADING);
         expect(response.text).toContain("1993");
         expect(response.text).toContain("08");
         expect(response.text).toContain("15");
+        expect(response.text).toContain("2021");
+        expect(response.text).toContain("09");
+        expect(response.text).toContain("10");
       });
 
       it("Should display director name on the page", async () => {
@@ -87,7 +91,7 @@ describe("Director date of birth controller tests", () => {
           lastName: "Smith"
         });
 
-        const response = await request(app).get(DIRECTOR_DATE_OF_BIRTH_URL);
+        const response = await request(app).get(DIRECTOR_DATE_DETAILS_URL);
   
         expect(response.text).toContain(PAGE_HEADING);
         expect(response.text).toContain("Jim Mid Smith");
@@ -97,22 +101,26 @@ describe("Director date of birth controller tests", () => {
 
     describe("post tests", () => {
 
-      it("Should redirect to director appointed date page", async () => {
+      it("Should redirect to director nationality page", async () => {
         mockGetValidationStatus.mockResolvedValueOnce(mockValidValidationStatusResponse);
         mockGetOfficerFiling.mockReturnValueOnce({
           referenceAppointmentId: APPOINTMENT_ID
         });
 
         const response = await request(app)
-          .post(DIRECTOR_DATE_OF_BIRTH_URL)
+          .post(DIRECTOR_DATE_DETAILS_URL)
           .send({ "dob_date-day": "15",
                   "dob_date-month": "08",
-                  "dob_date-year": "1993" });
+                  "dob_date-year": "1993",
+                  "appointment_date-day": "10",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "2021" });
 
-        expect(response.text).toContain("Found. Redirecting to " + DIRECTOR_APPOINTED_DATE_URL);
+        expect(response.text).toContain("Found. Redirecting to " + DIRECTOR_NATIONALITY_URL);
         expect(mockGetValidationStatus).toHaveBeenCalled();
         expect(mockPatchOfficerFiling).toHaveBeenCalledWith(expect.anything(), TRANSACTION_ID, SUBMISSION_ID, {
-          dateOfBirth: "1993-08-15"
+          dateOfBirth: "1993-08-15",
+          appointedOn: "2021-09-10"
         });
 
       });
@@ -131,10 +139,13 @@ describe("Director date of birth controller tests", () => {
         });
   
         const response = await request(app)
-          .post(DIRECTOR_DATE_OF_BIRTH_URL)
+          .post(DIRECTOR_DATE_DETAILS_URL)
           .send({ "dob_date-day": "15",
                   "dob_date-month": "08",
-                  "dob_date-year": "1993" });
+                  "dob_date-year": "1993",
+                  "appointment_date-day": "10",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "2021" });
   
         expect(response.text).toContain("You can only appoint a person as a director if they are at least 16 years old");
         expect(response.text).toContain("1993");
@@ -143,57 +154,127 @@ describe("Director date of birth controller tests", () => {
         expect(response.text).toContain("Jim Mid Smith");
         expect(mockGetValidationStatus).toHaveBeenCalled();
         expect(mockPatchOfficerFiling).toHaveBeenCalledWith(expect.anything(), TRANSACTION_ID, SUBMISSION_ID, {
-          dateOfBirth: "1993-08-15"
+          dateOfBirth: "1993-08-15",
+          appointedOn: "2021-09-10"
         });
       });
 
-      it("Should display error before patching if day is not a number", async () => {
+      it("Should display error before patching if dob day is not a number", async () => {
         jest.spyOn(apiEnumerations, 'lookupWebValidationMessage').mockReturnValueOnce("Date of birth must be a real date");
         mockGetOfficerFiling.mockReturnValueOnce({
           referenceAppointmentId: APPOINTMENT_ID
         });
   
         const response = await request(app)
-          .post(DIRECTOR_DATE_OF_BIRTH_URL)
+          .post(DIRECTOR_DATE_DETAILS_URL)
           .send({ "dob_date-day": "x",
                   "dob_date-month": "08",
-                  "dob_date-year": "1993" });
+                  "dob_date-year": "1993",
+                  "appointment_date-day": "10",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "2021" });
   
         expect(response.text).toContain("Date of birth must be a real date");
         expect(mockGetValidationStatus).not.toHaveBeenCalled();
         expect(mockPatchOfficerFiling).not.toHaveBeenCalled();
       });
   
-      it("Should display error before patching if month cannot exist", async () => {
+      it("Should display error before patching if dob month cannot exist", async () => {
         jest.spyOn(apiEnumerations, 'lookupWebValidationMessage').mockReturnValueOnce("Date of birth must be a real date");
         mockGetOfficerFiling.mockReturnValueOnce({
           referenceAppointmentId: APPOINTMENT_ID
         });
   
         const response = await request(app)
-          .post(DIRECTOR_DATE_OF_BIRTH_URL)
+          .post(DIRECTOR_DATE_DETAILS_URL)
           .send({ "dob_date-day": "15",
                   "dob_date-month": "15",
-                  "dob_date-year": "1993" });
+                  "dob_date-year": "1993",
+                  "appointment_date-day": "10",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "2021" });
   
         expect(response.text).toContain("Date of birth must be a real date");
         expect(mockGetValidationStatus).not.toHaveBeenCalled();
         expect(mockPatchOfficerFiling).not.toHaveBeenCalled();
       });
   
-      it("Should display error before patching if year is missing", async () => {
+      it("Should display error before patching if dob year is missing", async () => {
         jest.spyOn(apiEnumerations, 'lookupWebValidationMessage').mockReturnValueOnce("Date of birth must include a year");
         mockGetOfficerFiling.mockReturnValueOnce({
           referenceAppointmentId: APPOINTMENT_ID
         });
   
         const response = await request(app)
-          .post(DIRECTOR_DATE_OF_BIRTH_URL)
+          .post(DIRECTOR_DATE_DETAILS_URL)
           .send({ "dob_date-day": "15",
                   "dob_date-month": "08",
-                  "dob_date-year": "" });
+                  "dob_date-year": "",
+                  "appointment_date-day": "10",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "2021" });
   
         expect(response.text).toContain("Date of birth must include a year");
+        expect(mockGetValidationStatus).not.toHaveBeenCalled();
+        expect(mockPatchOfficerFiling).not.toHaveBeenCalled();
+      });
+
+      it("Should display error before patching if appointed day is not a number", async () => {
+        jest.spyOn(apiEnumerations, 'lookupWebValidationMessage').mockReturnValueOnce("Date the director was appointed must be a real date");
+        mockGetOfficerFiling.mockReturnValueOnce({
+          referenceAppointmentId: APPOINTMENT_ID,
+        });
+  
+        const response = await request(app)
+          .post(DIRECTOR_DATE_DETAILS_URL)
+          .send({ "appointment_date-day": "not a number",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "2021",
+                  "dob_date-day": "15",
+                  "dob_date-month": "08",
+                  "dob_date-year": "1993", });
+  
+        expect(response.text).toContain("Date the director was appointed must be a real date");
+        expect(mockGetValidationStatus).not.toHaveBeenCalled();
+        expect(mockPatchOfficerFiling).not.toHaveBeenCalled();
+      });
+  
+      it("Should display error before patching if appointed month cannot exist", async () => {
+        jest.spyOn(apiEnumerations, 'lookupWebValidationMessage').mockReturnValueOnce("Date the director was appointed must be a real date");
+        mockGetOfficerFiling.mockReturnValueOnce({
+          referenceAppointmentId: APPOINTMENT_ID,
+        });
+  
+        const response = await request(app)
+          .post(DIRECTOR_DATE_DETAILS_URL)
+          .send({ "appointment_date-day": "10",
+                  "appointment_date-month": "13",
+                  "appointment_date-year": "2021",
+                  "dob_date-day": "15",
+                  "dob_date-month": "08",
+                  "dob_date-year": "1993", });
+  
+        expect(response.text).toContain("Date the director was appointed must be a real date");
+        expect(mockGetValidationStatus).not.toHaveBeenCalled();
+        expect(mockPatchOfficerFiling).not.toHaveBeenCalled();
+      });
+  
+      it("Should display error before patching if appointed year is missing", async () => {
+        jest.spyOn(apiEnumerations, 'lookupWebValidationMessage').mockReturnValueOnce("Date the director was appointed must include a year");
+        mockGetOfficerFiling.mockReturnValueOnce({
+          referenceAppointmentId: APPOINTMENT_ID,
+        });
+  
+        const response = await request(app)
+          .post(DIRECTOR_DATE_DETAILS_URL)
+          .send({ "appointment_date-day": "10",
+                  "appointment_date-month": "09",
+                  "appointment_date-year": "",
+                  "dob_date-day": "15",
+                  "dob_date-month": "08",
+                  "dob_date-year": "1993", });
+  
+        expect(response.text).toContain("Date the director was appointed must include a year");
         expect(mockGetValidationStatus).not.toHaveBeenCalled();
         expect(mockPatchOfficerFiling).not.toHaveBeenCalled();
       });
