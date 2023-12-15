7b33cdf27400ef9d9e10048bbd71e321694d284e
olaych <oolatunji@companieshouse.gov.uk>
2023-12-15 13:05:17 +0000

    remove condition on browser back and add companyauthentication to ch01 routes

--- src/controllers/update/update.director.name.controller.ts
+++ src/controllers/update/update.director.name.controller.ts
@@ -1,10 +1,10 @@
 import { getDirectorName, postDirectorName } from "../../controllers/shared.controllers/director.name.controller"
 import { NextFunction, Request, Response } from "express"
-import { UPDATE_DIRECTOR_NAME_PATH, UPDATE_DIRECTOR_DETAILS_PATH } from "../../types/page.urls"
+import { UPDATE_DIRECTOR_DETAILS_PATH } from "../../types/page.urls"
 import { Templates } from "../../types/template.paths"
 
 export const get = (req: Request, resp: Response, next: NextFunction) => {
-  getDirectorName(req, resp, next, Templates.UPDATE_DIRECTOR_NAME, UPDATE_DIRECTOR_NAME_PATH, true);
+  getDirectorName(req, resp, next, Templates.UPDATE_DIRECTOR_NAME, UPDATE_DIRECTOR_DETAILS_PATH, true);
 }
 
 export const post = (req: Request, resp: Response, next: NextFunction) => {
