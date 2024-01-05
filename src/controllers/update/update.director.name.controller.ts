2674779130081e6d27f4b46b9f6c9be0a7aa5300
cdrainCH <cdrain@companieshouse.gov.uk>
2024-01-05 15:52:52 +0000

    addressing code smells

--- src/controllers/update/update.director.name.controller.ts
+++ src/controllers/update/update.director.name.controller.ts
@@ -1,6 +1,6 @@
 import { getDirectorName, postDirectorName } from "../../controllers/shared.controllers/director.name.controller"
 import { NextFunction, Request, Response } from "express"
-import { UPDATE_DIRECTOR_NAME_PATH, UPDATE_DIRECTOR_DETAILS_PATH, BASIC_STOP_PAGE_PATH, URL_QUERY_PARAM } from "../../types/page.urls"
+import { UPDATE_DIRECTOR_DETAILS_PATH, BASIC_STOP_PAGE_PATH, URL_QUERY_PARAM } from "../../types/page.urls"
 import { Templates } from "../../types/template.paths"
 import { urlUtils } from "../../utils/url"
 import { Session } from "@companieshouse/node-session-handler"
@@ -28,10 +28,10 @@ export const post = async (req: Request, resp: Response, next: NextFunction) => 
       return resp.redirect(
         urlUtils.setQueryParam(urlUtils.getUrlToPath(BASIC_STOP_PAGE_PATH, req), 
         URL_QUERY_PARAM.PARAM_STOP_TYPE, STOP_TYPE.ETAG));
-    };
+    }
   } catch(e) {
     return next(e);
-  };
+  }
 
   postDirectorName(req, resp, next, UPDATE_DIRECTOR_DETAILS_PATH, true);
 };
