869f320f8deef6642bde8480e3dd216a7ac5095f
Milan Desai <mdesai@companieshouse.gov.uk>
2024-01-09 11:24:05 +0000

    updated spacing

--- src/controllers/update/update.director.submitted.controller.ts
+++ src/controllers/update/update.director.submitted.controller.ts
@@ -12,33 +12,33 @@ import { getLocaleInfo, getLocalesService, selectLang } from "../../utils/locali
 import { CREATE_TRANSACTION_PATH } from "../../types/page.urls";
 
 export const get = async (req: Request, resp: Response, next: NextFunction) => {
-	try {
-		const companyNumber = urlUtils.getCompanyNumberFromRequestParams(req);
-		const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
-		const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
-		const session: Session = req.session as Session;
-		const companyProfile: CompanyProfile = await getCompanyProfile(companyNumber);
-		const officerFiling: OfficerFiling = await getOfficerFiling(session, transactionId, submissionId);
+  try {
+    const companyNumber = urlUtils.getCompanyNumberFromRequestParams(req);
+    const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
+    const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
+    const session: Session = req.session as Session;
+    const companyProfile: CompanyProfile = await getCompanyProfile(companyNumber);
+    const officerFiling: OfficerFiling = await getOfficerFiling(session, transactionId, submissionId);
 
-		const lang = selectLang(req.query.lang);
-		const locales = getLocalesService();
+    const lang = selectLang(req.query.lang);
+    const locales = getLocalesService();
 
-		return resp.render(Templates.UPDATE_DIRECTOR_SUBMITTED, {
-			templateName: Templates.UPDATE_DIRECTOR_SUBMITTED,
-			referenceNumber: transactionId,
-			companyNumber: companyNumber,
-			companyName: companyProfile.companyName,
-			directorName: formatTitleCase(retrieveDirectorNameFromFiling(officerFiling)),
-			updateDirectorSameCompany: urlUtils.getUrlToPath(CREATE_TRANSACTION_PATH, req),
-			nameHasBeenUpdated: officerFiling.nameHasBeenUpdated,
-			nationalityHasBeenUpdated: officerFiling.nationalityHasBeenUpdated,
-			occupationHasBeenUpdated: officerFiling.occupationHasBeenUpdated,
-			residentialAddressHasBeenUpdated: officerFiling.residentialAddressHasBeenUpdated,
-			correspondenceAddressHasBeenUpdated: officerFiling.correspondenceAddressHasBeenUpdated,
-			...getLocaleInfo(locales, lang),
-			currentUrl : req.originalUrl,
-		})
-	} catch (e) {
-		return next(e);
-	}
+    return resp.render(Templates.UPDATE_DIRECTOR_SUBMITTED, {
+      templateName: Templates.UPDATE_DIRECTOR_SUBMITTED,
+      referenceNumber: transactionId,
+      companyNumber: companyNumber,
+      companyName: companyProfile.companyName,
+      directorName: formatTitleCase(retrieveDirectorNameFromFiling(officerFiling)),
+      updateDirectorSameCompany: urlUtils.getUrlToPath(CREATE_TRANSACTION_PATH, req),
+      nameHasBeenUpdated: officerFiling.nameHasBeenUpdated,
+      nationalityHasBeenUpdated: officerFiling.nationalityHasBeenUpdated,
+      occupationHasBeenUpdated: officerFiling.occupationHasBeenUpdated,
+      residentialAddressHasBeenUpdated: officerFiling.residentialAddressHasBeenUpdated,
+      correspondenceAddressHasBeenUpdated: officerFiling.correspondenceAddressHasBeenUpdated,
+      ...getLocaleInfo(locales, lang),
+      currentUrl : req.originalUrl,
+    });
+  } catch (e) {
+    return next(e);
+  }
 }
