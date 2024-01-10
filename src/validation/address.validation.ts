207d5b0635fadd611ff0d6a0f062e7e0b2c0d335
AndreKramer7654 <akramer@companieshouse.gov.uk>
2024-01-10 15:57:39 +0000

    dact-1483 simplify valid roa test

--- src/validation/address.validation.ts
+++ src/validation/address.validation.ts
@@ -1,10 +1,7 @@
 import { RegisteredOfficeAddress } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
 
 export const validateRegisteredAddressComplete = (registeredOfficeAddress: RegisteredOfficeAddress | undefined): boolean => {
-  if (registeredOfficeAddress === undefined) {
-    return false;
-  }
-  return registeredOfficeAddress.premises !== undefined && registeredOfficeAddress.premises !== "" &&
+  return registeredOfficeAddress !== undefined && registeredOfficeAddress.premises !== undefined && registeredOfficeAddress.premises !== "" &&
     registeredOfficeAddress.addressLineOne !== undefined && registeredOfficeAddress.addressLineOne !== "" &&
     registeredOfficeAddress.locality !== undefined && registeredOfficeAddress.locality !== "" &&
     registeredOfficeAddress.postalCode !== undefined && registeredOfficeAddress.postalCode !== "" &&
