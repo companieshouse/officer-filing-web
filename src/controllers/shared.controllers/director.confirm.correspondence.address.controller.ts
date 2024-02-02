2209fcf704e61496f7ff82511fd35be3d5a0a1eb
leighmcclurg <lmcclurg@companieshouse.gov.uk>
2024-02-02 16:39:09 +0000

    Adding controllers and tests

---
+++ src/controllers/shared.controllers/director.confirm.correspondence.address.controller.ts
@@ -0,0 +1,59 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH,
+  DIRECTOR_RESIDENTIAL_ADDRESS_PATH,
+  UPDATE_DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH,
+  UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PATH
+} from "../../types/page.urls";
+import { urlUtils } from "../../utils/url";
+import { Session } from "@companieshouse/node-session-handler";
+import { getOfficerFiling, patchOfficerFiling } from "../../services/officer.filing.service";
+import { formatTitleCase } from "../../utils/format";
+import { OfficerFiling } from "@companieshouse/api-sdk-node/dist/services/officer-filing";
+import { getDirectorNameBasedOnJourney, setBackLink } from "../../utils/web";
+
+
+export const getConfirmCorrespondence = async (req: Request, res: Response, next: NextFunction, templateName: string, backUrlPath: string, isUpdate?: boolean) => {
+  try {
+    const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
+    const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
+    const session: Session = req.session as Session;
+    const officerFiling = await getOfficerFiling(session, transactionId, submissionId);
+    let enterAddressManuallyUrl: string;
+
+    if(isUpdate){
+      enterAddressManuallyUrl = urlUtils.getUrlToPath(UPDATE_DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH, req)
+    } else {
+      enterAddressManuallyUrl = urlUtils.getUrlToPath(DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH, req)
+  }
+
+    return res.render(templateName, {
+      templateName: templateName,
+      backLinkUrl: setBackLink(req, officerFiling.checkYourAnswersLink,urlUtils.getUrlToPath(backUrlPath, req)),
+      optionalBackLinkUrl: officerFiling.checkYourAnswersLink,
+      directorName: formatTitleCase(await getDirectorNameBasedOnJourney(isUpdate, session, req, officerFiling)),
+      enterAddressManuallyUrl: enterAddressManuallyUrl,
+      ...officerFiling.serviceAddress
+    });
+
+  } catch (e) {
+    return next(e);
+  }
+};
+
+export const postConfirmCorrespondence = async (req: Request, res: Response, next: NextFunction, nextPageUrl: string, isUpdate: boolean) => {
+  try {
+    const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
+    const submissionId = urlUtils.getSubmissionIdFromRequestParams(req);
+    const session: Session = req.session as Session;
+    
+    const officerFilingBody: OfficerFiling = {
+      isServiceAddressSameAsRegisteredOfficeAddress: false
+    };
+    
+    await patchOfficerFiling(session, transactionId, submissionId, officerFilingBody);
+      return res.redirect(urlUtils.getUrlToPath(nextPageUrl, req));
+  }catch(e){
+    return next(e);
+  }
+};
--- test/controllers/shared.controllers/director.confirm.correspondence.address.controller.unit.ts
+++ test/controllers/shared.controllers/director.confirm.correspondence.address.controller.unit.ts
@@ -1,27 +1,30 @@
-jest.mock("../../src/utils/feature.flag")
-jest.mock("../../src/services/officer.filing.service");
+jest.mock("../../../src/utils/feature.flag")
+jest.mock("../../../src/services/officer.filing.service");
+jest.mock("../../../src/services/company.appointments.service");
 
-import mocks from "../mocks/all.middleware.mock";
+import mocks from "../../mocks/all.middleware.mock";
 import request from "supertest";
-import app from "../../src/app";
+import app from "../../../src/app";
 
 import {
   DIRECTOR_CONFIRM_CORRESPONDENCE_ADDRESS_PATH,
-  DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH_END, DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_CHOOSE_ADDRESS_PATH,
-  DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_CHOOSE_ADDRESS_PATH_END, DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH,
   DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH_END,
   DIRECTOR_RESIDENTIAL_ADDRESS_PATH,
-  UPDATE_DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH,
+  UPDATE_DIRECTOR_CONFIRM_CORRESPONDENCE_ADDRESS_PATH,
   UPDATE_DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH_END,
+  UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PATH,
   urlParams
-} from "../../src/types/page.urls";
-import { getOfficerFiling, patchOfficerFiling } from "../../src/services/officer.filing.service";
-import { isActiveFeature } from "../../src/utils/feature.flag";
+} from "../../../src/types/page.urls";
+import { getOfficerFiling, patchOfficerFiling } from "../../../src/services/officer.filing.service";
+import { isActiveFeature } from "../../../src/utils/feature.flag";
+import { getCompanyAppointmentFullRecord } from "../../../src/services/company.appointments.service";
 
 const mockIsActiveFeature = isActiveFeature as jest.Mock;
 mockIsActiveFeature.mockReturnValue(true);
 const mockGetOfficerFiling = getOfficerFiling as jest.Mock;
 const mockPatchOfficerFiling = patchOfficerFiling as jest.Mock;
+const mockGetCompanyAppointmentFullRecord = getCompanyAppointmentFullRecord as jest.Mock;
+
 const COMPANY_NUMBER = "12345678";
 const TRANSACTION_ID = "11223344";
 const SUBMISSION_ID = "55555555";
@@ -31,31 +34,53 @@ const PAGE_URL = DIRECTOR_CONFIRM_CORRESPONDENCE_ADDRESS_PATH
   .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
   .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
   .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
-const NEXT_PAGE_URL = DIRECTOR_RESIDENTIAL_ADDRESS_PATH
+const UPDATE_PAGE_URL = UPDATE_DIRECTOR_CONFIRM_CORRESPONDENCE_ADDRESS_PATH
   .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
   .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
   .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
+const DIRECTOR_RESIDENTIAL_ADDRESS_PAGE_URL = DIRECTOR_RESIDENTIAL_ADDRESS_PATH
+  .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
+  .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
+  .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
+const UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PAGE_URL = UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PATH
+  .replace(`:${urlParams.PARAM_COMPANY_NUMBER}`, COMPANY_NUMBER)
+  .replace(`:${urlParams.PARAM_TRANSACTION_ID}`, TRANSACTION_ID)
+  .replace(`:${urlParams.PARAM_SUBMISSION_ID}`, SUBMISSION_ID);
+
 
 describe("Director confirm correspondence address controller tests", () => {
 
     beforeEach(() => {
       mocks.mockSessionMiddleware.mockClear();
       mockGetOfficerFiling.mockClear();
+       mockGetCompanyAppointmentFullRecord.mockClear();
     });
   
     describe("get tests", () => {
-  
-      it("Should navigate to director confirm correspondence address page", async () => {
-        mockGetOfficerFiling.mockResolvedValueOnce({
-          directorName: "John Smith"
-        })
-        const response = await request(app).get(PAGE_URL);
+
+        it.each([PAGE_URL,UPDATE_PAGE_URL])('should navigate to director confirm correspondence address page', async (url) => {
+            mockGetCompanyAppointmentFullRecord.mockResolvedValueOnce({
+                etag: "etag",
+                forename: "John",
+                otherForenames: "mid",
+                surname: "Smith"
+                 });
+
+            mockGetOfficerFiling.mockResolvedValueOnce({
+                directorName: "John Smith"
+                 });
+
+        const response = await request(app).get(url);
   
         expect(response.text).toContain(PAGE_HEADING);
         expect(mocks.mockCompanyAuthenticationMiddleware).toHaveBeenCalled();
       });
 
-      it("Should populate details on the page", async () => {
+      it.each([PAGE_URL,UPDATE_PAGE_URL])("Should populate details on the page", async (url) => {
+        mockGetCompanyAppointmentFullRecord.mockResolvedValueOnce({
+            etag: "etag",
+        });
+
         mockGetOfficerFiling.mockResolvedValueOnce({
           name: "John Smith",
           serviceAddress: {
@@ -68,9 +93,8 @@ describe("Director confirm correspondence address controller tests", () => {
             postalCode: "SW1A 2AA"
           }
         })
-        const response = await request(app).get(PAGE_URL);
+        const response = await request(app).get(url);
 
-        expect(response.text).toContain("John Smith");
         expect(response.text).toContain("110");
         expect(response.text).toContain("Test Line 1");
         expect(response.text).toContain("Downing Street");
@@ -80,66 +104,52 @@ describe("Director confirm correspondence address controller tests", () => {
         expect(response.text).toContain("SW1A 2AA");
       });
 
-      it("Should navigate back button to search page if officerFiling.correspondenceAddressBackLink includes " + DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH, async () => {
+      it.each([[PAGE_URL,DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH_END],[UPDATE_PAGE_URL,UPDATE_DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH_END]])("Should navigate back button to search page", async (url, backLink) => {
         mockGetOfficerFiling.mockResolvedValueOnce({
           directorName: "John Smith",
-          serviceAddressBackLink: "/director-correspondence-address-search"
         })
-        const response = await request(app).get(PAGE_URL);
-
-        expect(response.text).toContain(DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH_END);
+        const response = await request(app).get(url);
+        expect(response.text).toContain(backLink);
       });
 
-      it("Should navigate back button to choose address array page if officerFiling.correspondenceAddressBackLink includes " + DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH_END, async () => {
-        mockGetOfficerFiling.mockResolvedValueOnce({
-          directorName: "John Smith",
-          serviceAddressBackLink: "/director-correspondence-address-manual"
-        })
-        const response = await request(app).get(PAGE_URL);
-
-        expect(response.text).toContain(DIRECTOR_CORRESPONDENCE_ADDRESS_MANUAL_PATH_END);
-      });
-
-      it("Should navigate to error page when feature flag is off", async () => {
+      it.each([PAGE_URL,UPDATE_PAGE_URL])('Should navigate to error page when feature flag is off', async (url) => {
         mockIsActiveFeature.mockReturnValueOnce(false);
-        const response = await request(app).get(PAGE_URL);
-  
+        const response = await request(app).get(url);
         expect(response.text).toContain(ERROR_PAGE_HEADING);
-      });
 
-      it("should catch error if getofficerfiling error", async () => {
-        const response = await request(app).get(PAGE_URL);
+      });
+      
+      it.each([PAGE_URL,UPDATE_PAGE_URL])('should catch error if getofficerfiling error', async (url) => {
+        const response = await request(app).get(url);
         expect(response.text).not.toContain(PAGE_HEADING);
         expect(response.text).toContain(ERROR_PAGE_HEADING)
       });
 
-      it("should populate backLink parameter", async () => {
+      it.each([PAGE_URL,UPDATE_PAGE_URL])('should populate backLink parameter', async (url) => {
         mockGetOfficerFiling.mockResolvedValueOnce({
           directorName: "John Smith"
         })
         const response = await request(app).get(PAGE_URL);
-
         expect(response.text).toContain("backLink=confirm-correspondence-address");
       });
     });
 
     describe("post tests", () => {
   
-      it("Should redirect to residential address page", async () => {
-        const response = await request(app).post(PAGE_URL);
-
-        expect(response.text).toContain("Found. Redirecting to " + NEXT_PAGE_URL);
+      it.each([[PAGE_URL,DIRECTOR_RESIDENTIAL_ADDRESS_PAGE_URL],[UPDATE_PAGE_URL, UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PAGE_URL]])('Should redirect to residential address page', async (url, nextPageUrl) => {
+        const response = await request(app).post(url);
+        expect(response.text).toContain("Found. Redirecting to " + nextPageUrl);
         expect(mocks.mockCompanyAuthenticationMiddleware).toHaveBeenCalled();
       });
 
-      it("should catch error", async () => {
+      it.each([PAGE_URL,UPDATE_PAGE_URL])('should catch error', async (url) => {
         mockPatchOfficerFiling.mockRejectedValue(new Error())       
-        const response = await request(app).post(PAGE_URL);
+        const response = await request(app).post(url);
         expect(response.text).toContain(ERROR_PAGE_HEADING);
       });  
 
-      it("should set service address same as registered address to false", async () => {
-        const response = await request(app).post(PAGE_URL);
+      it.each([PAGE_URL,UPDATE_PAGE_URL])('should catch error', async (url) => {
+        const response = await request(app).post(url);
         mockPatchOfficerFiling.mockReturnValueOnce({
           data: {
             isServiceAddressSameAsRegisteredOfficeAddress: false
@@ -152,5 +162,3 @@ describe("Director confirm correspondence address controller tests", () => {
       }); 
     });
 });
-
-
