a90713c50f1978bb2734e82b80d2110f65bb4b18
olaych <oolatunji@companieshouse.gov.uk>
2024-02-12 09:55:26 +0000

    pipeline trigger

--- src/controllers/update/update.director.confirm.correspondence.address.controller.ts
+++ src/controllers/update/update.director.confirm.correspondence.address.controller.ts
@@ -7,4 +7,5 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     getConfirmCorrespondence(req, res, next, Templates.UPDATE_DIRECTOR_CONFIRM_CORRESPONDENCE_ADDRESS, UPDATE_DIRECTOR_CORRESPONDENCE_ADDRESS_SEARCH_PATH, true)}
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {   
-    postConfirmCorrespondence(req, res, next, UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PATH, true)}
+    postConfirmCorrespondence(req, res, next, UPDATE_DIRECTOR_RESIDENTIAL_ADDRESS_PATH, true)
+}
