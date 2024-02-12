4b7670eebd96e86d6600335079782013b89c26cd
jbestwickch <jbestwick@companieshouse.gov.uk>
2024-02-12 14:04:10 +0000

    Correct backlink path for residential search page

--- src/controllers/director.residential.address.search.controller.ts
+++ src/controllers/director.residential.address.search.controller.ts
@@ -1,12 +1,12 @@
 import { getDirectorResidentialAddressSearch, postDirectorResidentialAddressSearch } from "./shared.controllers/director.residential.address.search.controller"
 import { NextFunction, Request, Response } from "express"
-import { DIRECTOR_RESIDENTIAL_ADDRESS_MANUAL_PATH, DIRECTOR_RESIDENTIAL_ADDRESS_PATH } from "../types/page.urls"
+import { DIRECTOR_CORRESPONDENCE_ADDRESS_PATH, DIRECTOR_RESIDENTIAL_ADDRESS_MANUAL_PATH, DIRECTOR_RESIDENTIAL_ADDRESS_PATH } from "../types/page.urls"
 import { Templates } from "../types/template.paths"
 
 export const get = (req: Request, resp: Response, next: NextFunction) => {
-  getDirectorResidentialAddressSearch(req, resp, next, Templates.DIRECTOR_RESIDENTIAL_ADDRESS_SEARCH, {backLink: DIRECTOR_RESIDENTIAL_ADDRESS_PATH, manualEntryLink: DIRECTOR_RESIDENTIAL_ADDRESS_MANUAL_PATH}, false);
+  getDirectorResidentialAddressSearch(req, resp, next, Templates.DIRECTOR_RESIDENTIAL_ADDRESS_SEARCH, {backLink: DIRECTOR_CORRESPONDENCE_ADDRESS_PATH, manualEntryLink: DIRECTOR_RESIDENTIAL_ADDRESS_MANUAL_PATH}, false);
 }
 
 export const post = async (req: Request, resp: Response, next: NextFunction) => {
-  postDirectorResidentialAddressSearch(req, resp, next, Templates.DIRECTOR_RESIDENTIAL_ADDRESS_SEARCH, {backLink: DIRECTOR_RESIDENTIAL_ADDRESS_PATH, manualEntryLink: DIRECTOR_RESIDENTIAL_ADDRESS_MANUAL_PATH}, false);
+  postDirectorResidentialAddressSearch(req, resp, next, Templates.DIRECTOR_RESIDENTIAL_ADDRESS_SEARCH, {backLink: DIRECTOR_CORRESPONDENCE_ADDRESS_PATH, manualEntryLink: DIRECTOR_RESIDENTIAL_ADDRESS_MANUAL_PATH}, false);
 };
