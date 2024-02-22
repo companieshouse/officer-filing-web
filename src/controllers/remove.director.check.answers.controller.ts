1ab0cb2de89c09a5758d1dd663d6b669e13dadb8
leighmcclurg <lmcclurg@companieshouse.gov.uk>
2024-02-22 16:09:41 +0000

    tidying code

--- src/controllers/remove.director.check.answers.controller.ts
+++ src/controllers/remove.director.check.answers.controller.ts
@@ -19,7 +19,6 @@ import { getCompanyAppointmentFullRecord } from "../services/company.appointment
 import { getOfficerFiling } from "../services/officer.filing.service";
 import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
 
-
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const transactionId = urlUtils.getTransactionIdFromRequestParams(req);
