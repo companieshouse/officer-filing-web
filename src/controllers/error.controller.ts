7e6f84b181c08ddc66655b49594b35addd135d18
AndreKramer7654 <akramer@companieshouse.gov.uk>
2024-03-05 14:27:34 +0000

    dact-2002 error page i18n

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,6 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { Handler, NextFunction, Request, Response } from "express";
 import { logger } from "../utils/logger";
 import { Templates } from "../types/template.paths";
+import { selectLang, getLocalesService, getLocaleInfo } from "../utils/localise";
+import { urlUtils } from "../utils/url";
+import { SERVICE_ERROR_PATH } from "../types/page.urls";
 
 export const pageNotFound = (req: Request, res: Response) => {
   return res.status(404).render(Templates.ERROR_404, { templateName: Templates.ERROR_404 });
@@ -12,6 +15,24 @@ export const pageNotFound = (req: Request, res: Response) => {
  * Always keep this as the last handler in the chain for it to work.
  */
 export const errorHandler = (err: Error, req: Request, res: Response, _next: NextFunction) => {
+
   logger.errorRequest(req, `An error has occurred. Re-routing to the error screen - ${err.stack}`);
-  res.status(500).render(Templates.SERVICE_OFFLINE_MID_JOURNEY, { templateName: Templates.SERVICE_OFFLINE_MID_JOURNEY });
+
+  errorPage(req, res);
+};
+
+export const get: Handler = (req: Request, res: Response) => {
+  errorPage(req, res);
+};
+
+const errorPage = (req: Request, res: Response) => {
+  const lang = selectLang(req.query.lang);
+  const locales = getLocalesService();
+
+  res.status(500).render(Templates.SERVICE_OFFLINE_MID_JOURNEY,
+    {
+      ...getLocaleInfo(locales, lang),
+      currentUrl:  urlUtils.getUrlToPath(SERVICE_ERROR_PATH, req),
+      templateName: Templates.SERVICE_OFFLINE_MID_JOURNEY
+    });
 };
--- test/controllers/error.controller.unit.ts
+++ test/controllers/error.controller.unit.ts
@@ -3,9 +3,9 @@ jest.mock("../../src/utils/logger");
 import mocks from "../mocks/all.middleware.mock";
 import request from "supertest";
 import app from "../../src/app";
-import { NextFunction } from "express";
-import { CONFIRM_COMPANY_PATH } from "../../src/types/page.urls";
 import { logger } from "../../src/utils/logger";
+import { SERVICE_ERROR_PATH, CONFIRM_COMPANY_PATH } from "../../src/types/page.urls";
+import { NextFunction } from "express";
 
 const mockLoggerErrorRequest = logger.errorRequest as jest.Mock;
 
@@ -26,16 +26,32 @@ describe("Error controller test", () => {
     expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
   });
 
-//   it("Should render the error page", async () => {
-//     const message = "Can't connect";
-//     mocks.mockSessionMiddleware.mockImplementationOnce((req: Request, res: Response, next: NextFunction) => {
-//       return next(new Error(message));
-//     });
-//     const response = await request(app)
-//       .get(CONFIRM_COMPANY_PATH);
-//
-//     expect(response.status).toEqual(500);
-//     expect(response.text).toContain("Sorry, the service is unavailable");
-//     expect(mockLoggerErrorRequest.mock.calls[0][1]).toContain(message);
-//   });
+  it("should render the error page", async () => {
+
+    const response = await request(app)
+      .get(SERVICE_ERROR_PATH);
+
+    expect(response.text).toContain("Sorry, there is a problem with this service");
+  });
+
+
+  it("should render the error page in welsh", async () => {
+
+    const response = await request(app)
+      .get(SERVICE_ERROR_PATH + "?lang=cy");
+
+    expect(response.text).toContain("to be translated");
+  });
+
+  it("Should render the error page on error", async () => {
+    const message = "Can't connect";
+    mocks.mockSessionMiddleware.mockImplementationOnce((req: Request, res: Response, next: NextFunction) => {
+      return next(new Error(message));
+    });
+    const response = await request(app).get(CONFIRM_COMPANY_PATH);
+
+    expect(response.status).toEqual(500);
+    expect(response.text).toContain("Sorry, there is a problem with this service");
+    expect(mockLoggerErrorRequest.mock.calls[0][1]).toContain(message);
+  });
 });
