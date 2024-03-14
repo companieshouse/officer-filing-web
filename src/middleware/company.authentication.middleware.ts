6a6bacebf5888643ce13b11d341b168969e2c30d
AndreKramer7654 <117173564+AndreKramer7654@users.noreply.github.com>
2024-03-14 16:17:11 +0000

    Feature/dact 519 sanitize query params (#517)
    * dact-519 sanitize ids on urls
    
    * dact-519 fix up test names
    
    * dact-519 fix create transaction controller tests
    
    * dact-519 query params checks
    
    * dact-519 remove debug log
    
    * dact-519 sanitize stop type

--- src/middleware/company.authentication.middleware.ts
+++ src/middleware/company.authentication.middleware.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { CHS_URL } from "../utils/properties";
 import { urlParams } from "../types/page.urls";
+import { sanitizeCompanyNumber } from "../utils/url";
 
 export const companyAuthenticationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-  const companyNumber: string = req.params[urlParams.PARAM_COMPANY_NUMBER];
+  const companyNumber: string = sanitizeCompanyNumber(req.params[urlParams.PARAM_COMPANY_NUMBER]);
   const authMiddlewareConfig: AuthOptions = {
     chsWebUrl: CHS_URL,
     returnUrl: req.originalUrl,
