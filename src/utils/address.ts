2260fd75abffc61b24dc28dd479eaad0dd9f7efa
Milan Desai <mdesai@companieshouse.gov.uk>
2024-03-18 14:00:04 +0000

    DACT-2464 Fix a bug with false positive case where an empty string is evaluated as an update in the address field(s) * update the doc for getField().

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,31 +1,25 @@
 import { Address } from "@companieshouse/api-sdk-node/dist/services/officer-filing";
 /**
  * Determine whether two addresses are identical.
- * @param dateToConvert 
- * @param lang 
- * @returns true if the address' are identical
+ * @param addressOne
+ * @param addressTwo
+ * false positive, continue if an address element is undefined and another element is empty.
+ * @returns true if the address' are identical, false otherwise.
  */
 export const compareAddress = (addressOne: Address | undefined, addressTwo: Address | undefined): boolean => {
-  if (addressOne?.premises?.toLowerCase().trim() !== addressTwo?.premises?.toLowerCase().trim()){
-    return false;
-  }
-  if (addressOne?.addressLine1?.toLowerCase().trim() !== addressTwo?.addressLine1?.toLowerCase().trim()){
-    return false;
-  }
-  if (addressOne?.addressLine2?.toLowerCase().trim() !== addressTwo?.addressLine2?.toLowerCase().trim()){
-    return false;
-  }
-  if (addressOne?.country?.toLowerCase().trim() !== addressTwo?.country?.toLowerCase().trim()){
-    return false;
-  }
-  if (addressOne?.locality?.toLowerCase().trim() !== addressTwo?.locality?.toLowerCase().trim()){
-    return false;
-  }
-  if (addressOne?.postalCode?.toLowerCase().trim() !== addressTwo?.postalCode?.toLowerCase().trim()){
-    return false;
-  }
-  if (addressOne?.region?.toLowerCase().trim() !== addressTwo?.region?.toLowerCase().trim()){
-    return false;
+  const fields = ['premises', 'addressLine1', 'addressLine2', 'country', 'locality', 'postalCode', 'region'];
+
+  for (const field of fields) {
+    const valueOne = addressOne?.[field]?.toLowerCase().trim();
+    const valueTwo = addressTwo?.[field]?.toLowerCase().trim();
+
+    if ((valueOne === undefined && valueTwo === '') || (valueTwo === undefined && valueOne === '')) {
+      continue;
+    }
+
+    if (valueOne !== valueTwo) {
+      return false;
+    }
   }
   return true;
 };
