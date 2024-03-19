17c2afa979d5275c648a4725a7ff17cb0f9f5ea0
anona-w <117649383+anona-w@users.noreply.github.com>
2024-03-19 10:42:05 +0000

    DACT-1461: English WLE for Accessibility page

--- src/controllers/accessibility.statement.controller.ts
+++ src/controllers/accessibility.statement.controller.ts
@@ -1,13 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { Templates } from "../types/template.paths";
+import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const returnPageUrl = req.headers.referer!
+    const lang = selectLang(req.query.lang);
+    const locales = getLocalesService();
 
     try {
         return res.render(Templates.ACCESSIBILITY_STATEMENT, {
             templateName: Templates.ACCESSIBILITY_STATEMENT,
-            backLinkUrl: returnPageUrl
+            backLinkUrl: addLangToUrl(returnPageUrl, lang),
+            currentUrl: req.originalUrl,
+            ...getLocaleInfo(locales, lang)
         });
     } catch (e) {
         return next(e);
