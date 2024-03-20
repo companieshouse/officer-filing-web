7eb95a6b734ba71de58cb9213a914820051e28e3
anona-w <117649383+anona-w@users.noreply.github.com>
2024-03-20 16:11:40 +0000

    Fixed indentation

--- src/controllers/accessibility.statement.controller.ts
+++ src/controllers/accessibility.statement.controller.ts
@@ -6,22 +6,22 @@ import { logger } from "../utils/logger";
 import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-    const lang = selectLang(req.query.lang);
-    const locales = getLocalesService();
+  const lang = selectLang(req.query.lang);
+  const locales = getLocalesService();
 
-    const returnPage = addLangToUrl(getPreviousPageQueryParamUrl(req), lang);
-    logger.debugRequest(req, "Accessibility statement return page is " + returnPage);
+  const returnPage = addLangToUrl(getPreviousPageQueryParamUrl(req), lang);
+  logger.debugRequest(req, "Accessibility statement return page is " + returnPage);
 
-    const returnPageEncoded = encodeURIComponent(returnPage);
+  const returnPageEncoded = encodeURIComponent(returnPage);
 
-    try {
-        return res.render(Templates.ACCESSIBILITY_STATEMENT, {
-            templateName: Templates.ACCESSIBILITY_STATEMENT,
-            backLinkUrl: returnPage,
-            currentUrl: req.originalUrl + "?previousPage=" + returnPageEncoded,
-            ...getLocaleInfo(locales, lang)
-        });
-    } catch (e) {
-        return next(e);
-    }
+  try {
+    return res.render(Templates.ACCESSIBILITY_STATEMENT, {
+      templateName: Templates.ACCESSIBILITY_STATEMENT,
+      backLinkUrl: returnPage,
+      currentUrl: req.originalUrl + "?previousPage=" + returnPageEncoded,
+      ...getLocaleInfo(locales, lang)
+    });
+  } catch (e) {
+    return next(e);
+  }
 }
