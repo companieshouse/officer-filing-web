97b2205de65f8eeadcbbd3bcd56ff2a6a9b4d176
AndreKramer7654 <117173564+AndreKramer7654@users.noreply.github.com>
2024-03-22 15:20:52 +0000

    dact-1816 use company profile instead of stop screen validator (#526)
    * dact-1816 use company profile instead of stop screen validator
    
    * dact-1816 dateOfCessation added to sdk

--- src/services/stop.page.validation.service.ts
+++ src/services/stop.page.validation.service.ts
@@ -1,18 +1,8 @@
-import { Resource } from "@companieshouse/api-sdk-node";
-import { ApiErrorResponse } from "@companieshouse/api-sdk-node/dist/services/resource";
-import { Session } from "@companieshouse/node-session-handler";
-import { createPublicOAuthApiClient } from "./api.service";
-import { OfficerFilingService } from "@companieshouse/api-sdk-node/dist/services/officer-filing";
+import { getCompanyProfile } from "./company.profile.service";
 
-export const getCurrentOrFutureDissolved = async (session: Session, companyNumber: string): Promise<boolean> => {
-    const client = createPublicOAuthApiClient(session);
-    const ofService: OfficerFilingService = client.officerFiling;
-    const response: Resource<boolean> | ApiErrorResponse = await ofService.getCurrentOrFutureDissolved(companyNumber);
-    const status = response.httpStatusCode as number;
+export const getCurrentOrFutureDissolved = async (companyNumber: string): Promise<boolean> => {
 
-    if (status >= 400) {
-      throw new Error(`Error retrieving company information: ${JSON.stringify(response)}`);
-    }
-    const successfulResponse = response as Resource<boolean>;
-    return successfulResponse.resource as boolean;
+    const companyProfile = await getCompanyProfile(companyNumber);
+    
+    return !!companyProfile.dateOfCessation || companyProfile.companyStatus === "dissolved";
 }
