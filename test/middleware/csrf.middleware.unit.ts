3022af1eb373073d2dcf4dfe5374a6b298397dce
NeilShadrach <neil.shadrach@corryn.com>
2025-01-29 15:35:21 +0000

    SEC-90 Update test

--- test/middleware/csrf.middleware.unit.ts
+++ test/middleware/csrf.middleware.unit.ts
@@ -1,21 +1,27 @@
 import { CsrfError } from "@companieshouse/web-security-node";
 import { csrfErrorHandler } from "../../src/middleware/csrf.middleware";
-
+import { Request, Response, NextFunction } from "express";
 
 describe("csrfErrorHandler", () => {
+  let nextMock: jest.Mock;
+  let resMock: Partial<Response>;
+
   beforeEach(() => {
     jest.clearAllMocks();
+    nextMock = jest.fn();
+    resMock = {
+      status: jest.fn().mockReturnThis(),
+      render: jest.fn(),
+    };
   });
 
   it("calls next when not CSRF Error", () => {
-    const nextMock = jest.fn();
     const error = new Error("Non-CSRF error");
 
     csrfErrorHandler(
       error,
-      // @ts-expect-error  Not using the attributes of request so can be an empty object
-      {},
-      {},
+      {} as Request,
+      {} as Response,
       nextMock
     );
 
@@ -23,30 +29,18 @@ describe("csrfErrorHandler", () => {
   });
 
   it("renders 403 error when CSRF error", () => {
-    const nextMock = jest.fn();
     const error = new CsrfError("Token mismatch");
-
-    const resMock: {
-            status?: any,
-            render?: any
-        } = {};
-    resMock.status = jest.fn((_) => resMock);
-    resMock.render = jest.fn((_, __) => resMock);
 
     csrfErrorHandler(
       error,
-      // @ts-expect-error  Not using the attributes of request so can be an empty object
-      {},
-      resMock,
+      {} as Request,
+      resMock as Response,
       nextMock
     );
 
     expect(nextMock).not.toHaveBeenCalled();
     expect(resMock.status).toHaveBeenCalledWith(403);
-    expect(resMock.render).toHaveBeenCalledWith(
-      "csrf-error", {
-        csrfErrors: true
-      }
-    );
+    expect(resMock.render).toHaveBeenCalledWith("csrf-error", { csrfErrors: true });
   });
 });
+
