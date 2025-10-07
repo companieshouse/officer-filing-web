c927f30eb1bb46609e1a5f5ca0c5616d55cbdfb2
PhilipGlover <pglover@companieshouse.gov.uk>
2025-10-07 08:16:03 +0100

    add a test for the Service withdrawn, so that Sonar accepts test coverage

---
+++ test/controllers/start.controller.service.withdrawn.unit.ts
@@ -0,0 +1,29 @@
+import middlewareMocks from "../mocks/all.middleware.mock";
+import request from "supertest";
+import app from "../../src/app";
+
+jest.mock('../../src/utils/properties', () => ({
+  SERVICE_WITHDRAWN: "true",
+  CHS_URL: "http://chs.com",
+  PIWIK_START_GOAL_ID: "3",
+  EWF_URL: "https://ewf.companieshouse.gov.uk/",
+  TM01_ACTIVE: "true",
+  AP01_ACTIVE: "true",
+  CH01_ACTIVE: "true",
+}));
+
+describe("start controller SERVICE WITHDRAWN tests", () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return start page with Service Withdrawn message", async () => {
+    const response = await request(app)
+      .get("/appoint-update-remove-company-officer");
+
+    expect(response.text).toContain("You can currently only appoint, remove or update a company director online using our");
+    expect(middlewareMocks.mockAuthenticationMiddleware).not.toHaveBeenCalled();
+  });
+
+});
